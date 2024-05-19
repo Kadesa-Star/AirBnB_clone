cca2c39e592a6d71894ec60acf3b000d29dafbda
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-19 10:44:55 +0300

    Basemodel where all other classes inherit from

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,55 @@
 #!/usr/bin/python3
 import json
+from uuid import uuid4
+from datetime import datetime
+import os
 
 class BaseModel:
     """this class defines all common attributes/methods for other classes"""
-    
-
-    def __init__(self, id, created_at, updated_at) -> None:
-        self.id = str(id)
-        self.created_at = str(created_at)
-        self.updated_at = str(updated_at)
+    #serial_objc = {}
+    def __init__(self) -> None:
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self) -> str:
-        return f"id: {self.id} created_at: {self.created_at} updated_at: {self.updated_at}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    
+    # here we save our data in a file
+    def save(self) -> None:
+        self.updated_at = datetime.now()
+    """
+        self.serial_objc[self.new_k()] = self.to_dict()
+        with open('file.json', "w", indent = 2) as file:
+            json.dump(self.serial_objc, file)
+    """
 
     # this returns a json object to dictionary
     def to_dict(self):
         to_json = self.__dict__
+        to_json['__class__'] = self.__class__.__name__
+        to_json['created_at'] = to_json['created_at'].isoformat()
+        to_json['updated_at'] = to_json['updated_at'].isoformat()
         return to_json
 
-    # Serialization
-    def save(self):
-        with open('file.json', "w", indent = 2) as file:
-            json.dump(self.__dict__(), file)
+    """def new_k(self):
+        key = f"{self.__class__.__name__}.{self.id}"
+        return key
 
-
-
+    def reload(self):
+        with open('file.json', "r", indent = 2) as file:
+            if os.path.getsize('file.json') > 0:
+                prev_data = json.load(file)
+                # print(prev_data)
+                    for i, j in prev_data.items():
+                    self.serial_objc[i] = j
+"""
 
 # instances of the class
-User1 = Basemodel(45, june 1, june 2)
+User1 = BaseModel()
 print(User1)
-User1.save()
+#User1.reload()
+#User1.save()
 
 
