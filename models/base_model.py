10271aff5e04d5e65bd6dea9fd563650aa45272a
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-19 18:34:44 +0300

    this is an update to the base model to include kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -9,13 +9,20 @@ from datetime import datetime
 class BaseModel:
     """this class defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize the Base Model
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
