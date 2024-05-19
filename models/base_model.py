884f87cc6118d2b8fa7a1bff3e2cb733834d920c
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-19 17:23:18 +0300

    This is the BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -32,6 +32,7 @@ class BaseModel:
         """
         to_json = {
             '__class__': self.__class__.__name__,
+            'id': self.id,
             'created_at': self.created_at.isoformat(),
             'updated_at': self.updated_at.isoformat(),
         }
