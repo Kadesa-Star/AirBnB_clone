1a6dec08d9f944318f52958cd40770cd2c3c0919
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-19 15:52:59 +0300

    This is the base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,57 +1,33 @@
 #!/usr/bin/python3
 """This is the Base Class all other  classes inherit from it"""
-import json
 from uuid import uuid4
 from datetime import datetime
-import os
 
 
 class BaseModel:
     """this class defines all common attributes/methods for other classes"""
 
     # serial_objc = {}
-    def __init__(self) -> None:
+    def __init__(self):
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-    # here we save our data in a file
-    def save(self) -> None:
-
+    def save(self):
+        """
+        update the updated_at attribute with the current datetime
+        """
         self.updated_at = datetime.now()
-    """
-        self.serial_objc[self.new_k()] = self.to_dict()
-        with open('file.json', "w", indent = 2) as file:
-            json.dump(self.serial_objc, file)
-    """
 
-    # this returns a json object to dictionary
     def to_dict(self):
-        to_json = self.__dict__
+        """
+        Return a dictionary representation of instance
+        """
+        to_json = self.__dict__.copy()
         to_json['__class__'] = self.__class__.__name__
         to_json['created_at'] = to_json['created_at'].isoformat()
         to_json['updated_at'] = to_json['updated_at'].isoformat()
         return to_json
-
-    """def new_k(self):
-        key = f"{self.__class__.__name__}.{self.id}"
-        return key
-
-    def reload(self):
-        with open('file.json', "r", indent = 2) as file:
-            if os.path.getsize('file.json') > 0:
-                prev_data = json.load(file)
-                # print(prev_data)
-                    for i, j in prev_data.items():
-                    self.serial_objc[i] = j
-"""
-
-
-# instances of the class
-User1 = BaseModel()
-print(User1)
-# User1.reload()
-User1.save()
