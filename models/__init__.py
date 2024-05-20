8b2b7c73ed2335ad1e114117dcfc665150189ba8
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-20 23:09:39 +0300

    this is my init.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+This magic method belongs to the models directory
+"""
 from models.engine.file_storage import FileStorage
+
+
 storage = FileStorage()
-storage.reload()
+storage.reload(i)
