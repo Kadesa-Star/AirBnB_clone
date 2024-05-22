7460a9a68deb85df8c21ed7cad5e337e5ae2edda
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-05-22 06:44:43 +0300

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This is the Base Class all other  classes inherit from it"""
 
-import uuid
+from uuid import uuid4
 from datetime import datetime
 import models
 
@@ -13,6 +13,10 @@ class BaseModel:
         """
         Initialize the Base Model
         """
+
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
@@ -20,9 +24,6 @@ class BaseModel:
                 if k != "__class__":
                     setattr(self, k, v)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
